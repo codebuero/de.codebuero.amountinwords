d43118dc66db07e2679e329da1aff06466e2a1bd
arotter <andreas.rotter.kontakt@gmail.com>
2018-02-01 12:45:37 +0100

    remove comments, format code

--- plugins/function.amountinwords.php
+++ plugins/function.amountinwords.php
@@ -5,22 +5,22 @@ define('NUMERAL_HUNDREDS_SUFFIX', 'hundert');
 define('NUMERAL_INFIX', 'und');
 
 function num2text($pNumber) {
-     $lNumeral = array('null', 'ein', 'zwei', 'drei', 'vier',
-                  'fünf', 'sechs', 'sieben', 'acht', 'neun',
-                  'zehn', 'elf', 'zwölf', 'dreizehn', 'vierzehn',
-                  'fünfzehn', 'sechzehn', 'siebzehn', 'achtzehn', 'neunzehn');
-    
-    if ($pNumber == 0) {
-        return $lNumeral[0]; // „null“
-    } elseif ($pNumber < 0) {
-        return NUMERAL_SIGN . ' ' . num2text_group(abs($pNumber));
-    } else {
-        return num2text_group($pNumber);
-    }
+  $lNumeral = array('null', 'ein', 'zwei', 'drei', 'vier',
+                'fünf', 'sechs', 'sieben', 'acht', 'neun',
+                'zehn', 'elf', 'zwölf', 'dreizehn', 'vierzehn',
+                'fünfzehn', 'sechzehn', 'siebzehn', 'achtzehn', 'neunzehn');
+
+  if ($pNumber == 0) {
+    return $lNumeral[0]; // „null“
+  } elseif ($pNumber < 0) {
+    return NUMERAL_SIGN . ' ' . num2text_group(abs($pNumber));
+  } else {
+    return num2text_group($pNumber);
+  }
 }
 
 /**
- * Rekursive Methode, die das Zahlwort zu einer Ganzzahl zurÃ¼ckgibt.
+ *
  * @global array $lNumeral
  * @global array $lTenner
  * @global array $lGroupSuffix
@@ -28,14 +28,12 @@ function num2text($pNumber) {
  * @param int $pGroupLevel (optional) Das Gruppen-Level der aktuellen Zahl.
  * @return string Das Zahlwort.
  */
-function num2text_group($pNumber, $pGroupLevel = 0)
-{
-   
+function num2text_group($pNumber, $pGroupLevel = 0) {
   $lNumeral = array('null', 'ein', 'zwei', 'drei', 'vier',
                   'fünf', 'sechs', 'sieben', 'acht', 'neun',
                   'zehn', 'elf', 'zwölf', 'dreizehn', 'vierzehn',
                   'fünfzehn', 'sechzehn', 'siebzehn', 'achtzehn', 'neunzehn');
- 
+
   $lTenner = array('', '', 'zwanzig', 'dreißig', 'vierzig',
                  'fünfzig', 'sechzig', 'siebzig', 'achtzig', 'neunzig');
 
@@ -47,53 +45,42 @@ function num2text_group($pNumber, $pGroupLevel = 0)
                       array('e Billiarde ', ' Billiarden '),
                       array('e Trillion ', ' Trillionen '));
 
+  if ($pNumber == 0) {
+    return '';
+  }
 
- 
-    /* Ende der Rekursion ist erreicht, wenn Zahl gleich Null ist */
-    if ($pNumber == 0) {
-        return '';
+  $lGroupNumber = $pNumber % 1000;
+
+  if ($lGroupNumber == 1) {
+    $lResult = $lNumeral[1] . $lGroupSuffix[$pGroupLevel][0];
+  } elseif ($lGroupNumber > 1) {
+    $lResult = '';
+
+    $lFirstDigit = floor($lGroupNumber / 100);
+
+    if ($lFirstDigit > 0) {
+      $lResult .= $lNumeral[$lFirstDigit] . NUMERAL_HUNDREDS_SUFFIX;
     }
-    
-    /* Zahlengruppe dieser Runde bestimmen */
-    $lGroupNumber = $pNumber % 1000;
-    
-    /* Zahl der Zahlengruppe ist Eins */
-    if ($lGroupNumber == 1) {
-        $lResult = $lNumeral[1] . $lGroupSuffix[$pGroupLevel][0]; // â€žeine Milliardeâ€œ
-        
-    /* Zahl der Zahlengruppe ist grÃ¶ÃŸer als Eins */   
-    } elseif ($lGroupNumber > 1) {
-        $lResult = '';
-        
-        /* Zahlwort der Hunderter */
-        $lFirstDigit = floor($lGroupNumber / 100);
-        
-        if ($lFirstDigit > 0) {
-            $lResult .= $lNumeral[$lFirstDigit] . NUMERAL_HUNDREDS_SUFFIX; // â€žfÃ¼nfhundertâ€œ
-        }
-        
-        /* Zahlwort der Zehner und Einer */
-        $lLastDigits = $lGroupNumber % 100;
-        $lSecondDigit = floor($lLastDigits / 10);
-        $lThirdDigit = $lLastDigits % 10;
-        
-        if ($lLastDigits == 1) {
-            $lResult .= $lNumeral[1] . 's'; // "eins"
-        } elseif ($lLastDigits > 1 && $lLastDigits < 20) {
-            $lResult .= $lNumeral[$lLastDigits]; // "dreizehn"
-        } elseif ($lLastDigits >= 20) {
-            if ($lThirdDigit > 0) {
-                $lResult .= $lNumeral[$lThirdDigit] . NUMERAL_INFIX; // "sechsundâ€¦"
-            }
-            $lResult .= $lTenner[$lSecondDigit]; // "â€¦achtzig"
-        }
-        
-        /* Suffix anhÃ¤ngen */
-        $lResult .= $lGroupSuffix[$pGroupLevel][1]; // "Millionen"
+
+    $lLastDigits = $lGroupNumber % 100;
+    $lSecondDigit = floor($lLastDigits / 10);
+    $lThirdDigit = $lLastDigits % 10;
+
+    if ($lLastDigits == 1) {
+      $lResult .= $lNumeral[1] . 's';
+    } elseif ($lLastDigits > 1 && $lLastDigits < 20) {
+      $lResult .= $lNumeral[$lLastDigits];
+    } elseif ($lLastDigits >= 20) {
+      if ($lThirdDigit > 0) {
+        $lResult .= $lNumeral[$lThirdDigit] . NUMERAL_INFIX;
+      }
+      $lResult .= $lTenner[$lSecondDigit];
     }
-    
-    /* NÃ¤chste Gruppe auswerten und Zahlwort zurÃ¼ckgeben */
-    return num2text_group(floor($pNumber / 1000), $pGroupLevel + 1) . $lResult;
+
+    $lResult .= $lGroupSuffix[$pGroupLevel][1];
+  }
+
+  return num2text_group(floor($pNumber / 1000), $pGroupLevel + 1) . $lResult;
 }
 
 function _parse_amount($input) {
@@ -101,7 +88,7 @@ function _parse_amount($input) {
     $pre = num2text((int) $input);
     $post = num2text(($input - floor($input)) * 100);
     return ucfirst($pre) . ' Euro ' . ucfirst($post) . ' Cent';
-  } 
+  }
   return '';
 }
 
@@ -115,7 +102,7 @@ function smarty_function_amountinwords($params){
     trigger_error("amountinwords: missing value for parameter 'value'");
     return;
   }
-  
+
   $out = _parse_amount($params['value']);
   return $out;
 }
